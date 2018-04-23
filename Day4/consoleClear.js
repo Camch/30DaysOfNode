7783dc4c6bbc2e012e9349dcf5666eedd3086e9c
Ho <camilla.ho@sogeti.com>
2018-04-23 13:36:43 +0200

    First excerises of Day4

---
+++ Day4/consoleClear.js
@@ -0,0 +1,7 @@
+var value = 10;
+console.log("Value: %d", value);
+
+console.clear(); //this will clear it all
+value *= value;
+
+console.log("Value: %d", value);
---
+++ Day4/consoleError.js
@@ -0,0 +1,10 @@
+var x = 10;
+var y = 20;
+
+var result = y/x;
+
+if (result == 2){
+    console.log("Result: %d",result);
+} else {
+    console.log("Error: Error in Positioning Operands ")
+}
