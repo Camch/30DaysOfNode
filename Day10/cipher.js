f1c755c1eb9c4d2ef9033a99cdeaa6b06efe910e
Camch <skycam2@gmail.com>
2018-04-24 15:13:45 +0200

    Day10 started

---
+++ Day10/cipher.js
@@ -0,0 +1,25 @@
+
+var crypto = require('crypto'),algorithm = 'aes-256-ctr',password = 'RJ23edrf';
+
+function encrypt(text) {
+    var cipher = crypto.createCipher(algorithm, password);
+    var crypted = cipher.update(text, 'utf-8', 'hex');
+    crypted += cipher.final('hex');
+    return crypted;
+}
+
+
+function decrypt(text) {
+    var decipher = crypto.createDecipher(algorithm,password);
+    var dec = decipher.update(text,'hex','utf-8');
+    dec += decipher.final('utf-8');
+    return dec;
+}
+
+var text = "Nodejsera for all web development languages";
+
+var e = encrypt(text);
+console.log(e);
+
+var d = decrypt(e);
+console.log(d);
